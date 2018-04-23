baedb47e96e65a83919032c1e3f89559e739a82b
DESKTOP-OTLBKFE\HuuThien <vuonghuuthientk@gmail.com>
2018-04-23 22:42:15 +0700

    Update User

--- user/js/index.js
+++ user/js/index.js
@@ -1,4 +1,7 @@
 
+//var number = require('./fn/number');
+//import { PI } from "./fn/number";
+
 function buttonChange()
 {
     var email = document.getElementById("email_us").value;
@@ -20,6 +23,24 @@ function buttonChange()
         {
             alert("Password and Confirm Password are not the same.");
             return false;
+        }
+    }
+}
+
+function dateClick(index)
+{
+    var groupDates = document.querySelectorAll('div.paper div.background div.history div.groupDate');
+    for (var i = 0; i < groupDates.length ; i++) 
+    {
+        if(groupDates[index].className === "groupDate none")
+        {
+            groupDates[index].className = "groupDate block";
+            return;
+        }
+        else
+        {
+            groupDates[index].className = "groupDate none";
+            return;
         }
     }
 }
