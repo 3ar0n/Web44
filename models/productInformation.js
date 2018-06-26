6c3be7f122c2cb977959af7b201f71237f789480
Đặng Quốc Thái <dangquocthai316@gmail.com>
2018-06-27 00:43:44 +0700

    fix, update SQL

--- models/productInformation.js
+++
@@ -1,7 +0,0 @@
-var database = require('../database/database');
-var config = require('../config/config');
-
-exports.loadAll = () => {
-    var sql = `select * from productinformation where proID = 0`;
-    return database.load(sql);
-}
