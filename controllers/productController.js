d5cbdefd1a723b43129114cc0693c730c0272ded
Ngọc Hoàng <giusehoang.263@gmail.com>
2018-06-26 21:09:14 +0700

    update productinformation

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,18 @@
 const express = require('express');
+var productInformation = require('../models/productInformation');
+var productModel = require('../models/productModel');
 const router = express.Router();
 
 router.get('/', function(req,res){
-	res.render('product/product-information');
+	productInformation.loadAll().then(pi_rows => {
+		productModel.loadInformation().then(pm_rows => {
+			var vm = {
+				information: pi_rows,
+				model: pm_rows
+			}
+			res.render('product/product-information', vm);
+		})
+	})
 })
 
 module.exports = router;
--- database/web44.sql
+++ database/web44.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 25, 2018 at 04:20 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 26, 2018 lúc 01:19 PM
+-- Phiên bản máy phục vụ: 10.1.33-MariaDB
+-- Phiên bản PHP: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `web44`
+-- Cơ sở dữ liệu: `web44`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `brands`
+-- Cấu trúc bảng cho bảng `brands`
 --
 
 CREATE TABLE `brands` (
@@ -34,7 +34,7 @@ CREATE TABLE `brands` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `brands`
+-- Đang đổ dữ liệu cho bảng `brands`
 --
 
 INSERT INTO `brands` (`brandID`, `brandName`) VALUES
@@ -59,7 +59,7 @@ INSERT INTO `brands` (`brandID`, `brandName`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Cấu trúc bảng cho bảng `categories`
 --
 
 CREATE TABLE `categories` (
@@ -68,7 +68,7 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `categories`
+-- Đang đổ dữ liệu cho bảng `categories`
 --
 
 INSERT INTO `categories` (`catID`, `catName`) VALUES
@@ -78,7 +78,41 @@ INSERT INTO `categories` (`catID`, `catName`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Cấu trúc bảng cho bảng `productinformation`
+--
+
+CREATE TABLE `productinformation` (
+  `proID` int(11) NOT NULL,
+  `anhSP` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `slide` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ManHinh` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `HDH` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cameraTruoc` int(11) DEFAULT NULL,
+  `CameraSau` int(11) DEFAULT NULL,
+  `CPU` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `RAM` int(11) DEFAULT NULL,
+  `BoNhoTrong` int(11) DEFAULT NULL,
+  `SIM` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Pin` int(11) DEFAULT NULL,
+  `TenSP` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `brandID` int(11) NOT NULL,
+  `Gia` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `productinformation`
+--
+
+INSERT INTO `productinformation` (`proID`, `anhSP`, `slide`, `ManHinh`, `HDH`, `cameraTruoc`, `CameraSau`, `CPU`, `RAM`, `BoNhoTrong`, `SIM`, `Pin`, `TenSP`, `brandID`, `Gia`) VALUES
+(0, 'iphone-x-256gb.png', 'slide-iphoneX', 'OLED, 5.8\", Super Retina', 'iOS 11', 7, 12, 'Apple A11 Bionic 6 nhân', 3, 256, '1 Nano SIM, Hỗ trợ 4G', 2716, 'iPhone X - 256GB', 0, 34790000),
+(1, 'bkav-bphone-2-hero.png', 'slide-bphone-2', 'IPS LCD, 5.5\", Full HD', 'BOS (Android 7.1)', 8, 16, 'Snapdragon 625 8 nhân 64-bit', 3, 32, '2 Nano SIM, Hỗ trợ 4G', 3000, 'BPhone 2017', 16, 9790000),
+(2, 'htc-u-play.png', 'htc-u-play', 'Super LCD, 5.2\", Full HD', 'Android 6.0 (Marshmallow)', 16, 16, 'MTK Helio P10 8 nhân 64-bit', 3, 32, '2 Nano SIM, Hỗ trợ 4G', 2500, 'HTC U Play', 5, 3790000),
+(3, 'nokia-6.png', 'nokia-6', 'IPS LCD, 5.5\", Full HD', 'Android 7.0 (Nougat)', 8, 16, 'Qualcomm Snapdragon 430 8 nhân 64 bit', 3, 32, '\r\n2 SIM Nano (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G', 3000, 'Nokia 6', 10, 4990000);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
 --
 
 CREATE TABLE `products` (
@@ -97,7 +131,7 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `products`
+-- Đang đổ dữ liệu cho bảng `products`
 --
 
 INSERT INTO `products` (`proID`, `proName`, `brandID`, `catID`, `imgLink`, `price`, `shortInfo`, `fullInfo`, `addedDay`, `totalView`, `totalBought`, `inStorage`) VALUES
@@ -109,7 +143,7 @@ INSERT INTO `products` (`proID`, `proName`, `brandID`, `catID`, `imgLink`, `pric
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sessions`
+-- Cấu trúc bảng cho bảng `sessions`
 --
 
 CREATE TABLE `sessions` (
@@ -119,23 +153,29 @@ CREATE TABLE `sessions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `brands`
+-- Chỉ mục cho bảng `brands`
 --
 ALTER TABLE `brands`
   ADD PRIMARY KEY (`brandID`);
 
 --
--- Indexes for table `categories`
+-- Chỉ mục cho bảng `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`catID`);
 
 --
--- Indexes for table `products`
+-- Chỉ mục cho bảng `productinformation`
+--
+ALTER TABLE `productinformation`
+  ADD PRIMARY KEY (`proID`);
+
+--
+-- Chỉ mục cho bảng `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`proID`),
@@ -143,17 +183,17 @@ ALTER TABLE `products`
   ADD KEY `brandID` (`brandID`);
 
 --
--- Indexes for table `sessions`
+-- Chỉ mục cho bảng `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`session_id`);
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `products`
+-- Các ràng buộc cho bảng `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`catID`) REFERENCES `categories` (`catID`),
