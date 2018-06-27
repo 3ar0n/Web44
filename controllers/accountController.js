04c3f3a8ff2426d730001827e324d613a1be7fb5
Vuong Huu Thien <vuonghuuthientk@gmail.com>
2018-06-27 11:19:30 +0700

    fix view sign in + sign up

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('account/index');
+router.get('/signin', (req, res) => {
+    res.render('account/signin');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('account/signup');
 });
 
 module.exports = router;
