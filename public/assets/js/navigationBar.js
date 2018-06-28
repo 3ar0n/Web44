af81f6b26a9d796a01f3ca4a6462690b8e23d83f
Vuong Huu Thien <vuonghuuthientk@gmail.com>
2018-06-29 02:47:52 +0700

    edit navigationBar + write profile

--- public/assets/js/navigationBar.js
+++ public/assets/js/navigationBar.js
@@ -6,3 +6,27 @@ function dropdownNav() {
         x.className = "navigation-bar";
     }
 }
+
+function brandFunction() {
+    document.getElementById("brandDropdown").classList.toggle("show");
+}
+
+function typeFunction() {
+    document.getElementById("typeDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(e) {
+    if (!e.target.matches('.brandButton')) {
+        var myDropdown = document.getElementById("brandDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+  if (!e.target.matches('.typeButton')) {
+        var myDropdown = document.getElementById("typeDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+  }
+}
